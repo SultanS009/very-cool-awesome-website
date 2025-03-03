870691aeb331841ca96f5a8f96cd396ad2248e42
SultanS009 <sultan.salama000@gmail.com>
2025-03-03 18:18:27 +0000

    Mon, Mar 3, 2025, 10:18 AM -08:00

---
+++ js/event.js
@@ -0,0 +1,10 @@
+const element1 = document.getElementById('element1')
+const element2 = document.getElementById('element2')
+const element3 = document.getElementById('element3')
+const element4 = document.getElementById('element4')
+const element5 = document.getElementById('element5')
+
+
+element1.addEventListner('click', ()=>{
+    element1.innerHTML = "hey"
+})
