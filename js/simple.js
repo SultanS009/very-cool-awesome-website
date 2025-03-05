5d50973adac400c56ddd716dcbc0d6d57d46ff20
SultanS009 <sultan.salama000@gmail.com>
2025-03-05 20:17:25 +0000

    Wed, Mar 5, 2025, 12:17 PM -08:00

--- js/simple.js
+++ js/simple.js
@@ -1,1 +1,37 @@
-.l.lv     
+const element3 = document.getElementById('element3')
+
+let positionY = 0;
+let positionX = 0;
+
+document.addEventListener('keyup', (event)=>{
+    console.log(event.key)
+
+    if (event.key == 'ArrowDown')
+    {
+        positionY = positionY + 10
+    }
+    else if (event.key == 'ArrowUp')
+    {
+        positionY = positionY - 10
+    }
+    else if (event.key == 'ArrowLeft')
+    {
+        positionX = positionX + 10
+    }
+    else if (event.key == 'ArrowRight')
+    {
+        positionX = positionX - 10
+    }
+    element3.style.top = `${positionY}px`
+    element3.style.right = `${positionX}px`
+
+})
+
+const list = ['running', 'paused']
+let index = 0
+element3.addEventListener('click', ()=>{
+    element3.style.animationPlayState = list[index]
+    index = (index + 1) % 2
+})
+
+
