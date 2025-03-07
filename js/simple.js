7b20ae1679266381fa980eabbce7e17e45bab1b4
SultanS009 <sultan.salama000@gmail.com>
2025-03-07 17:34:22 +0000

    Fri, Mar 7, 2025, 9:34 AM -08:00

---
+++ js/simple.js
@@ -0,0 +1,7 @@
+const element1 = document.getElementById('element1')
+
+element1.addEventListener('click', ()=>{
+    element1.innerHTML = "Click me to enable light mode."
+    element1.style.color = white
+    element1.style.backgroundColor = black
+})
