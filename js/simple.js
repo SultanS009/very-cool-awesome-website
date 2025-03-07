30fc6522487b8060ec1cc6564c175476558fd4ff
SultanS009 <sultan.salama000@gmail.com>
2025-03-07 17:40:23 +0000

    Fri, Mar 7, 2025, 9:40 AM -08:00

--- js/simple.js
+++ js/simple.js
@@ -1,7 +1,13 @@
 const element1 = document.getElementById('element1')
 
 element1.addEventListener('click', ()=>{
-    element1.innerHTML = "<h3>Click me to enable light mode.</h3>"
-    element1.style.color = black
-    element1.style.backgroundColor = white
+    element1.innerHTML = "<h3>Double click me to enable light mode.</h3>"
+    element1.style.color = "black"
+    element1.style.backgroundColor = "white"
+})
+
+element1.addEventListener('dblclick', ()={
+    element1.innerHTML = "<h3> Click me to enable light mode.</h3>"
+    element1.style.color = "white"
+    element1.style.backgroundColor = "black"
 })
