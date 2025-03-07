42161d74a5e85d2692ae0a3c4edd6a66aa8ec471
SultanS009 <sultan.salama000@gmail.com>
2025-03-07 17:50:41 +0000

    Fri, Mar 7, 2025, 9:50 AM -08:00

--- js/simple.js
+++ js/simple.js
@@ -1,13 +1,23 @@
 const element1 = document.getElementById('element1')
 
-element1.addEventListener('click', ()=>{
-    element1.innerHTML = "<h3>Click me to enable dark mode.</h3>"
-    element1.style.color = "black"
-    element1.style.backgroundColor = "white"
+document.addEventListener('keyup', (event)=>{
+    console.log(event.key)
+    if (event.key == 'd')
+    {
+        element1.innerHTML = "<h3>Click 'l' to enable light mode.</h3>"
+        element1.style.color = "black"
+        element1.style.backgroundColor = "white"
+        body.style.backgroundColor = "black"
+    }
 })
 
-element1.addEventListener('dblclick', ()={
-    element1.innerHTML = "<h3>Click me to enable light mode.</h3>"
-    element1.style.color = "white"
-    element1.style.backgroundColor = "black"
+document.addEventListener('keyup', (event)=>{
+    console.log(event.key)
+    if (event.key == 'l')
+    {
+        element1.innerHTML = "<h3>Click 'd' to enable light mode.</h3>"
+        element1.style.color = "white"
+        element1.style.backgroundColor = "black"
+        body.style.backgroundColor = "white"
+    }
 })
