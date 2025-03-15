ac5c3619db8ccb522303d2cc40df07022c35cc7d
SultanS009 <sultan.salama000@gmail.com>
2025-03-15 20:25:51 +0000

    Sat, Mar 15, 2025, 1:25 PM -07:00

--- js/nav.js
+++ js/nav.js
@@ -1,1 +1,14 @@
-co
+nav = document.querySelector('.nav')
+
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if (window.scrollY > 223)
+    {
+        nav.classList.add('active')
+    }
+    else
+    {
+        nav.classList.remove('active')
+    }
+}
