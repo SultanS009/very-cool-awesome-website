221b18ec5369f292e6787ab2d68c8f7ec7acd1af
SultanS009 <sultan.salama000@gmail.com>
2025-03-15 20:40:20 +0000

    Sat, Mar 15, 2025, 1:40 PM -07:00

--- js/nav.js
+++ js/nav.js
@@ -3,7 +3,7 @@ nav = document.querySelector('.nav')
 window.addEventListener('scroll', fixNav)
 
 function fixNav() {
-    if (window.scrollY > 223)
+    if (window.scrollY > 270)
     {
         nav.classList.add('active')
     }
