f26ffe087bb5afaa142f1cb94be179842cab7b13
SultanS009 <sultan.salama000@gmail.com>
2025-04-09 04:29:27 +0000

    Tue, Apr 8, 2025, 9:29 PM -07:00

--- js/ball.js
+++ js/ball.js
@@ -1,15 +1,30 @@
-const ball = document.createElement('div')
-const ballRadius = 50
 const windowHeight = window.innerHeight
 const windowWidth = window.innerWidth
+const ball = document.createElement('div')
+document.body.appendChild(ball)
+const LPadel = document.createElement('div')
+document.body.appendChild(LPadel)
+let LPadelWidth = 20
+let LPadelHeight = 100
+let LPadelSpeed = 1
+let LPadelYPosition = windowHeight / 2 - ballRadius
+const ballRadius = 50
 let ballXPosition = windowWidth / 2 - ballRadius
 let ballXDirection = 1
 let ballYPosition = windowHeight / 2 - ballRadius
 let ballYDirection = 1
 let ballSpeed = 5
 
+let score = 0 // Display score and increase the score by 1 everytime the ball hits the paddel.
+let level = 1 // Display the level and increase the level by 1 everytime the score increases by 10.
+// As level increases, increase the ball speed and make it more difficult
+// If the ball gets past the paddel, end the game.
+// make the ball stop or disapear and let the user know game over
+// Optional: sound effects, background music, color change for every bounce.
+
 setInterval(moveBall, 10)
 setInterval(moveBallUp, 10)
+
 
 function moveBall(){
     ballXPosition = ballXPosition + ballSpeed * ballXDirection
@@ -18,6 +33,13 @@ function moveBall(){
     {
         ballXDirection = ballXDirection * -1
     }
+
+    let ballTop = ballYPosition
+    let ballBottom = ballYPosition + 2 * ballRadius
+    let ballLeft = ballXPosition
+    let LPadelTop = LPadelYPosition
+    let LPadelBottom = LPadelYPosition + LPadelHeight
+    let LPadelRight = LPadelXPosition + LPadelWidth
 }
 
 function moveBallUp(){
@@ -29,9 +51,11 @@ function moveBallUp(){
     }
 }
 
+if (ball)
+
 createBall()
 function createBall(){
-    document.body.appendChild(ball)
+
     ball.style.height = `${2 * ballRadius}px`
     ball.style.width = `${2 * ballRadius}px`
     ball.style.borderRadius = "50%"
@@ -39,4 +63,40 @@ function createBall(){
     ball.style.position = "absolute"
     ball.style.top = `${windowHeight / 2 - ballRadius}px`
     ball.style.left = `${windowWidth / 2 - ballRadius}px`
+}
+
+createLPadel()
+function createLPadel(){
+    LPadel.style.height = `${LPadelHeight}px`
+    LPadel.style.width = `${LPadelWidth}px`
+    LPadel.style.backgroundColor = 'blue'
+    LPadel.style.position = 'absolute'
+    LPadel.style.left = "50px"
+    LPadel.style.top = `${LPadelYPosition}px`
+}
+
+document.addEventListener('keyup', (event) => {
+    if (event.key == 'w')
+    {
+        if (LPadelYPosition <= 0) {
+            LPadelYPosition = 0
+        }
+        else {
+            LPadelYPosition = LPadelYPosition - LPadelSpeed
+        }
+    }
+    if (event.key == 's')
+    {
+        if (LPadelYPosition >= windowHeight - LPadelHeight) {
+            LPadelYPosition = windowHeight - LPadelHeight
+        }
+        else {
+            LPadelYPosition = LPadelYPosition + LPadelSpeed
+        }
+    }
+    LPadel.style.top = `${LPadelYPosition}px`
+})
+function movePadel(){
+
+
 }
