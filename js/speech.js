e00b90ff978ca4c298e9f24635cc497379f57ca7
SultanS009 <sultan.salama000@gmail.com>
2025-05-16 17:15:53 +0000

    Fri, May 16, 2025, 10:15 AM -07:00

---
+++ js/speech.js
@@ -0,0 +1,46 @@
+const voiceSelect = document.getElementById('voices')
+const textarea = document.getElementById('')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data.forEach(createBox)
+
+function createBox(item) {
+    console.log(item)
+}
