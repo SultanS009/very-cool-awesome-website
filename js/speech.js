753f5a67971e11a98d542fb3ad716477b5ed5980
SultanS009 <sultan.salama000@gmail.com>
2025-05-21 16:39:43 +0000

    Wed, May 21, 2025, 9:39 AM -07:00

--- js/speech.js
+++ js/speech.js
@@ -48,5 +48,41 @@ function createBox(item) {
         <img src="${item.image}" alt="${item.tex}"/>
         <p class="info"> ${item.text} </p>
     `
+
+    box.addEventListener('click', () => {
+        setTextMessage(text)
+        speakText()
+    })
+
     main.appenedChild(box)
 }
+
+
+// toggle text box
+toggleBtn.addEventListener('click', () => {
+    document.getElementById('text-box').classList.toggle('show')
+})
+
+// close text box
+closeBtn.addEventListener('click', () => {
+    document.getElementById('text-box').classList.remove('show')
+})
+
+let voices = []
+
+function getVoices() {
+    vocies = speechSynthesis.getVoices()
+
+    voices.forEach(voice => {
+        const option = document.createElement('option')
+        option.value = voice.name
+        option.innerText = `${voice.name} ${voice.lang}`
+        voicesSelect.appendChild(option)
+    })
+}
+
+
+//voices change
+speechSynthesis.addEventListener('voiceschanged', getVoices)
+
+getVoices()
