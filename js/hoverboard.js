0632593836c5bcfb50373ef79b5e79e01db5d003
SultanS009 <sultan.salama000@gmail.com>
2025-05-28 16:08:14 +0000

    Wed, May 28, 2025, 9:08 AM -07:00

---
+++ js/hoverboard.js
@@ -0,0 +1,4 @@
+// Java Script Time
+const container = document.getElementById('container')
+const colors = ["red", "purple", "green", "blue", "yellow", "indigo", "hotpink", "orange", "white", "darkgreen", "maroon", "darkblue"]
+const SQUARES = 500
