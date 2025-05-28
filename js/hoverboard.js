e45bc5669e9899c08d1bd7d5335c117c948c559b
SultanS009 <sultan.salama000@gmail.com>
2025-05-28 16:11:39 +0000

    Wed, May 28, 2025, 9:11 AM -07:00

--- js/hoverboard.js
+++ js/hoverboard.js
@@ -2,3 +2,9 @@
 const container = document.getElementById('container')
 const colors = ["red", "purple", "green", "blue", "yellow", "indigo", "hotpink", "orange", "white", "darkgreen", "maroon", "darkblue"]
 const SQUARES = 500
+
+for(let i = 0; i < SQUARES; i++)
+{
+    const square = document.createElement('div')
+    square.classList.add('square')
+}
