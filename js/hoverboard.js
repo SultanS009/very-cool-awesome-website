c41fad6db41b6a64b5be7fa718380517004cbb77
SultanS009 <sultan.salama000@gmail.com>
2025-05-28 16:19:45 +0000

    Wed, May 28, 2025, 9:19 AM -07:00

--- js/hoverboard.js
+++ js/hoverboard.js
@@ -12,5 +12,11 @@ for(let i = 0; i < SQUARES; i++)
 }
 
 function setColor(element){
-    
+    const color = getRandomColor()
+    element.style.background = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function getRandomColor(){
+    return colors[0]
 }
