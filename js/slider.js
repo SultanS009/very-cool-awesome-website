9dec07fa677af8afca63745df841c71e8298fc56
SultanS009 <sultan.salama000@gmail.com>
2025-05-28 21:53:14 -0700

    some

--- js/slider.js
+++ js/slider.js
@@ -2,34 +2,34 @@ const upButton = document.querySelector('.up-button')
 const downButton = document.querySelector('.down-button')
 const rightSide = document.querySelector('.right-side')
 const leftSide = document.querySelector('.left-side')
-const slidesLength = rightSide.querySelectorAll('.div').length
+const slidesLength = rightSide.querySelectorAll('div').length
 
 //first slide
-currentSlide = 1 //slideLength = 4
-leftSide.style.top = `${-(slideLenght - currentSlide)*100}%`
+let currentSlide = 1 //slidesLength = 4
+leftSide.style.top = `${-(slidesLength - currentSlide)*100}%`
 
 upButton.addEventListener('click', () => changeSlide('up'))
 downButton.addEventListener('click', () => changeSlide('down'))
 
 function changeSlide(direction) {
-    if (direction = 'up')
+    if (direction == 'up')
     {
-        if (currentSlide == slideLength)
+        if (currentSlide == slidesLength)
         {
             currentSlide = 0
         }
         currentSlide = currentSlide + 1
 
     }
-    else (direction = 'down')
+    else if (direction == 'down')
     {
         currentSlide = currentSlide - 1
         if (currentSlide == 0)
         {
-            currentSlide = slideLength + 1
+            currentSlide = slidesLength + 1
         }
     }
-    leftSide.style.top = `${-(slideLenght - currentSlide)*100}%`
+    leftSide.style.top = `${-(slidesLength - currentSlide)*100}%`
     rightSide.style.top = `${-(currentSlide - 1) * 100}%`
 }
 
